adb55670235450216ae5ac0005329ec9a81fb482
Travis Chen <381404825@qq.com>
2016-11-03 16:34:18 +0800

    fix spelling typo

--- seg_ftxt.py
+++ seg_ftxt.py
@@ -11,7 +11,7 @@ segutil = segjb.SegJb()
 segutil.init(stopwords_file=seg_dir+'/stopwords.dat',
              puncs_file=seg_dir+'/punctuations.dat',
              user_dict=seg_dir+'/newdict.dat',
-             adsilent=True)
+             silent=True)
 segutil.set_param(delim=' ', ngram=2, keep_stopwords=True, keep_puncs=False)
 
 for line in sys.stdin:
